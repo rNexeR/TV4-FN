fe84046c075f9f04498baf98e6187e84b2ad76b3
NexeRodriguez <rnexer2995@gmail.com>
2014-09-19 21:36:17 -0600

    Initial Project
    Proyecto Inicial

---
+++ ProgramacionClases/src/team/Player.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team;
+
+/**
+ *
+ * @author KELVIN
+ */
+public class Player {
+    public int dorsal;
+    public String nombre;
+    public double salario;
+    public boolean dispo;
+    
+    public Player(int d, String n, double s){
+        dorsal = d;
+        nombre = n;
+        salario = s;
+        dispo = true;
+    }
+    
+    
+}
